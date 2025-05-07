d3ed10d3527797953325ba7ad09f064f36ff847a
Pixel-Arni <office@pixelarni.de>
2025-05-07 09:03:26 +0200

    try 5

--- start.js
+++ start.js
@@ -6,22 +6,10 @@ module.exports = {
       params: {
         venv: "env",
         message: [
-          "uvicorn backend.main:app --port 8000"
-        ],
-        on: [{
-          event: /http:\/\/\S+/,
-          done: true
-        }]
-      }
-    },
-    {
-      method: "shell.run",
-      params: {
-        message: [
           "cd frontend && npm run dev"
         ],
         on: [{
-          event: /http:\/\/\S+/,
+          event: /http:\/\/localhost:5173/,
           done: true
         }]
       }
@@ -33,4 +21,4 @@ module.exports = {
       }
     }
   ]
-};
+}
