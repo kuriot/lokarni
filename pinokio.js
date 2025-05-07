82b55783056d45b71401b547214558038e6c51f9
Pixel-Arni <office@pixelarni.de>
2025-05-07 11:43:01 +0200

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,21 +1,19 @@
-// pinokio.js
 module.exports = {
   title: "Lokarni",
   icon: "lokarni-icon.png",
   menu: async (kernel, info) => {
     const installed = await info.exists("env");
-    const isRunning = await kernel.isRunning("start.js");
 
     return [
       {
         text: "Installieren",
         href: "install.js",
-        default: !installed // Wichtig für Erstinstallation
+        default: !installed // <- ganz wichtig für Erstinstallation
       },
       {
         text: "Start",
         href: "start.js",
-        default: installed && !isRunning
+        default: installed // <- Standardaktion nach Installation
       },
       {
         text: "Stop",
