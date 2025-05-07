a1a8e6e313ac7ab64ddbbc41e11307a8ee927f96
Pixel-Arni <office@pixelarni.de>
2025-05-07 12:07:27 +0200

    Update install.js

--- install.js
+++ install.js
@@ -4,7 +4,7 @@ module.exports = {
       method: "shell.run",
       params: {
         venv: "env",
-        message: ["pip install -r requirements.txt"],
+        message: ["pip install -r requirements.txt"]
       }
     },
     {
