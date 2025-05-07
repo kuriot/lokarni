4ae844b2861edf788b0c2a884d5f3b50af1f7fa6
Pixel-Arni <office@pixelarni.de>
2025-05-07 12:12:20 +0200

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -2,39 +2,33 @@ module.exports = {
   title: "Lokarni",
   icon: "lokarni-icon.png",
   menu: async (kernel, info) => {
-    const isInstalled = await info.exists("env");
-    const isRunning = await kernel.isRunning("start.js");
-
-    const menu = [];
-
-    if (!isInstalled) {
-      // Nur wenn nicht installiert, zeige "Installieren" und öffne automatisch
-      menu.push({
+    const installed = await info.exists("env");
+    return [
+      {
         text: "Installieren",
         href: "install.js",
-        default: true
-      });
-    }
-
-    if (isInstalled) {
-      menu.push({
-        text: isRunning ? "Stop" : "Start",
-        href: isRunning ? "stop.js" : "start.js",
-        default: !isRunning // Start nur aktiv, wenn nicht läuft
-      });
-
-      menu.push({
-        text: "Update",
-        href: "update.js"
-      });
-
-      menu.push({
-        text: "Zurücksetzen",
-        href: "reset.js"
-      });
-    }
-
-    return menu;
+        default: !installed   // sorgt für automatische Ausführung bei Erststart
+      },
+      ...(installed ? [
+        {
+          text: "Start",
+          href: "start.js",
+          default: true
+        },
+        {
+          text: "Stop",
+          href: "stop.js"
+        },
+        {
+          text: "Update",
+          href: "update.js"
+        },
+        {
+          text: "Zurücksetzen",
+          href: "reset.js"
+        }
+      ] : [])
+    ];
   },
   url: async (kernel) => kernel.local.get("url")
 };
