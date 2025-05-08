14b3457a19ebceaa1e4f0bf5a56d407c16b37f73
Pixel-Arni <office@pixelarni.de>
2025-05-08 06:43:04 +0200

    New Start  Files

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting Lokarni backend..."
-cd backend
-uvicorn main:app --host 127.0.0.1 --port 8000 &
-cd ../frontend
-echo "Installing frontend dependencies..."
-npm install
-echo "Starting frontend..."
-npm run dev
